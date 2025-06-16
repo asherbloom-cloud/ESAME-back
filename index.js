253400d9d0b129d68de2c4820b9237f859f816bc
asherbloom-cloud <federicodibenedetto4@gmail.com>
2025-06-16 20:39:56 +0200

    port

--- index.js
+++ index.js
@@ -47,6 +47,7 @@ app.get('/api/weather', async (req, res) => {
   }
 })
 
-app.listen(3000, () => {
-  console.log('✅ Server avviato su http://localhost:3000')
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`✅ Server avviato sulla porta ${PORT}`)
 })
